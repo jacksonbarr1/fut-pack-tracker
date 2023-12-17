68b23be0dfb7ffda4b5202459b39049c7dc5d680
Jackson Barr <jbarr39@gatech.edu>
2023-12-17 17:36:30 -0500

    Add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from "./components/NavBar.jsx";
+import MyPacks from "./components/MyPacks.jsx";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Statistics from "./components/Statistics.jsx";
+import Settings from "./components/Settings.jsx";
+
 
 function App() {
 
   return (
-    <>
-        <h1>Fut Pack Tracker</h1>
-    </>
+    <Router>
+        <NavBar />
+        <Routes>
+            <Route path="/packs" element={<MyPacks />} />
+            <Route path="/statistics" element={<Statistics />} />
+            <Route path="/settings" element={<Settings />} />
+        </Routes>
+    </Router>
   )
 }
 
